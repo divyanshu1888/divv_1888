52aa2c834de722ac28812b6d17816eb830da3809
divyanshu1888 <warriordivyanshu@gmail.com>
2026-03-16 22:05:42 +0530

    Update example.java

--- Divyanshu/example.java
+++ Divyanshu/example.java
@@ -1,1 +1,40 @@
-
+import java.util.Scanner;
+public class example {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int n1=sc.nextInt();
+        System.out.print("Enter second number:");
+        int n2=sc.nextInt();
+        
+        System.out.println("""
+        Enter choices as follows:
+            1 for addition
+            2 for subtraction
+            3 for multiplication
+            4 for division
+        """);
+        int key=sc.nextInt();
+        switch(key){
+            case 1:
+                System.out.print(n1+n2);
+                break;
+            
+            case 2:
+                System.out.print(n1-n2);
+                break;
+            
+            case 3:
+                System.out.print(n1*n2);
+                break;
+            
+            case 4:
+                System.out.print(n1/n2);
+                break;
+            
+            default:
+                System.out.println("Invalid entry");
+        
+        }
+    }
+}
